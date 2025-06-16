cae3efe1f758df4aad462675375f237e28ef2e1a
andrelisboa1 <51169222+andrelisboa1@users.noreply.github.com>
2025-06-16 11:05:20 +0100

    Update smartstorage.lua

--- smartstorage.lua
+++ smartstorage.lua
@@ -1,9 +1,10 @@
 local monitor = peripheral.find("monitor")
 
 
-function writeToMonitor(x, y, txt)
+function writeToMonitor(x, y, txt, color)
+  monitor.setTextColor(color)
   monitor.setCursorPos(x, y)
   monitor.write(txt)
 end
 
-writeToMonitor(1, 1, "Hello!")
+writeToMonitor(1, 1, "[ Welcome to the ]", colors.yellow)
